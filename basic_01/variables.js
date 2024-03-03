f80d9ca630f16bc6e2f86a02ed75c30e5f274b94
Ankit Rai <118017567+luck122de@users.noreply.github.com>
2024-03-03 03:40:42 +0000

    types of  variables in javascript

---
+++ basic_01/variables.js
@@ -0,0 +1,11 @@
+const accountID=1234;
+let accountEmail="safe@g,ail.com";
+var accountPassword="12345";
+accountCity="lucknow";
+let accountState;
+
+accountEmail="luck@123";
+accountPassword="6543";
+accountCity="delhi";
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
